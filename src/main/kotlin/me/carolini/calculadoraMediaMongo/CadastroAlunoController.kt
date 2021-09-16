6e91373656b748f5e900268af2dd67d160912a97
CaroliniKauffmann <carolini.kauffmann@gmail.com>
2021-09-16 15:43:47 -0300

    Controller - Cadastro Aluno

---
+++ src/main/kotlin/me/carolini/calculadoraMediaMongo/CadastroAlunoController.kt
@@ -0,0 +1,20 @@
+package me.carolini.calculadoraMediaMongo
+
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/cadastro/aluno")
+class CadastroAluno {
+
+    @PostMapping("/novo")
+    fun cadastroAluno(@RequestBody dadosDoAluno: DadosDoAluno): ArrayList<String> {
+        val aluno = Aluno()
+
+        aluno.preencherNome(dadosDoAluno.nome)
+        aluno.preencherAno(dadosDoAluno.ano)
+        aluno.preencherNotas(dadosDoAluno.notas)
+
+        return arrayListOf(aluno.mostrarResultadoMedia(), aluno.verificarAprovacao(7))
+    }
+
+}
