861fcacd4ba4d0acd84d49a00999c3b0641164d4
CaroliniKauffmann <carolini.kauffmann@gmail.com>
2021-09-16 15:37:09 -0300

    Criando Repositorio

---
+++ src/main/kotlin/me/carolini/calculadoraMediaMongo/Aluno.kt
@@ -0,0 +1,49 @@
+package me.carolini.calculadoraMediaMongo
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.util.*
+import kotlin.collections.ArrayList
+
+@Document
+class Aluno {
+    @Id
+    var id: ObjectId = ObjectId.get()
+    private var nome = "string"
+    private var ano = 0
+    private var notas: ArrayList<Float> = ArrayList()
+
+
+    fun preencherNome(nome:String){
+        this.nome = nome
+    }
+
+    fun preencherAno(ano:Int) {
+        this.ano = ano
+    }
+
+    fun preencherNotas(notas:ArrayList<Float>) {
+        this.notas = notas
+
+    }
+    private fun calcularMedia(): Float {
+
+        val totalDeNotas = notas.size
+        val soma = notas.sum()
+        return soma / totalDeNotas
+    }
+
+    fun mostrarResultadoMedia(): String {
+        return "$nome, do ano $ano, teve a média ${calcularMedia()}"
+    }
+
+    fun verificarAprovacao(mediaAprovacao:Int): String {
+        if (calcularMedia() >= mediaAprovacao) {
+            return "Você foi aprovado!"
+        } else {
+            return "Você foi reprovado!"
+        }
+
+    }
+}
---
+++ src/main/kotlin/me/carolini/calculadoraMediaMongo/CalculadoraMediaMongoApplication.kt
@@ -0,0 +1,11 @@
+package me.carolini.calculadoraMediaMongo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CalculadoraMediaMongoApplication
+
+fun main(args: Array<String>) {
+	runApplication<CalculadoraMediaMongoApplication>(*args)
+}
