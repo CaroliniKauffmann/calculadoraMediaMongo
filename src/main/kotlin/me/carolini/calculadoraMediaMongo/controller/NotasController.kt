3abbec27d5a03cc362a35686ee356def9ba237cc
CaroliniKauffmann <carolini.kauffmann@gmail.com>
2021-10-01 10:50:13 -0300

    Função de deletar aluno

--- src/main/kotlin/me/carolini/calculadoraMediaMongo/controller/NotasController.kt
+++ src/main/kotlin/me/carolini/calculadoraMediaMongo/controller/NotasController.kt
@@ -1,27 +1,32 @@
 package me.carolini.calculadoraMediaMongo.controller
 import me.carolini.calculadoraMediaMongo.model.AnoModel
 import me.carolini.calculadoraMediaMongo.model.NotasModel
+import me.carolini.calculadoraMediaMongo.repository.AnoRepository
 import me.carolini.calculadoraMediaMongo.resquest.NotasRequest
 import me.carolini.calculadoraMediaMongo.repository.NotasRepository
+import org.bson.types.ObjectId
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/cadastro/notas")
-class NotasController (private val notasRepository: NotasRepository) {
+class NotasController (private val notasRepository: NotasRepository, val anoRepository: AnoRepository) {
 
     @PostMapping("/nova")
     fun cadastroAluno(@RequestBody request: NotasRequest): ResponseEntity<NotasModel> {
         val notasModel = NotasModel(
             notas = request.notas
         )
-        notasModel.ano = AnoModel(request.anoID)
+        notasModel.ano = anoRepository.findOneById(request.anoID)
         val notas = notasRepository.save(notasModel)
         return ResponseEntity(notas, HttpStatus.CREATED)
     }
-
+    @DeleteMapping("/deletarNota/{notaID}")
+    fun deletarNotas(@PathVariable notaID: ObjectId): ResponseEntity<String> {
+        if (notasRepository.existsById(notaID)) {
+            notasRepository.deleteById(notaID)
+        }
+        return ResponseEntity("Nota deletada com sucesso!", HttpStatus.OK)
+    }
 }
